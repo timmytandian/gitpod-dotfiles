6b36ed683041b04fc3f8593adbbbdff7edd76c23
timmytandian <37656192+timmytandian@users.noreply.github.com>
2024-08-12 14:18:51 +0900

    Exclude some files from setup.sh
    Exclude "deno_configure.sh" and "gcloud_cli_install.sh" from the install list.

--- setup.sh
+++ setup.sh
@@ -1,15 +1,35 @@
 #!/bin/bash
 
+# Define current directory and make a temporary directory
 TMPDIR=$(mktemp -d)
-
 CURRENT=$PWD
 
+# Go to the temp dir to perform execute each installation task.
+# Each installation task may produce some files (e.g. unzipping
+# or downloading files), so we can remove these unnecessary
+# files easily later.
 cd $TMPDIR
 
+# Define a list of installation scripts to exclude
+EXCLUDES=("deno_configure.sh" "gcloud_cli_install.sh")
+
+# Run each installation script inside the "scripts" folder.
+# Gitpod clones this repo into "$HOME/.dotfiles" folder in the
+# Gitpod development instance.
 for script in ~/.dotfiles/scripts/*; do
+  script_name=$(basename "$script")
+  
+  # Check if the script is in the exclusion list
+  if [[ " ${EXCLUDES[@]} " =~ " ${script_name} " ]]; then
+    continue
+  fi
+
   bash "$script"
 done
 
+
+# Go back to the initial directory
 cd $CURRENT
 
+# Clean up the temp dir
 rm -rf $TMPDIR
