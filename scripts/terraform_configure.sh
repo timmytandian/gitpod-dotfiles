e321c4d5af1fbb70631ee86a3e92cf77b38679e9
timmy tandian <timmytandian@gmail.com>
2024-08-18 15:04:22 +0000

    Changed the installation script to be simpler; without prompt.

--- scripts/terraform_configure.sh
+++ scripts/terraform_configure.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
-
 if command -v /usr/bin/terraform &> /dev/null; then
     exit
 fi
 
-sudo apt-get update && sudo apt-get install -y gnupg software-properties-common curl
 
-curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
-
-sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
-
-sudo apt-get update && sudo apt-get install terraform
+# Installation reference: https://developer.hashicorp.com/terraform/install#linux
+wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+sudo apt update && sudo apt install terraform
